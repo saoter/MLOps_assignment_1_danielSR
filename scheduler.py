307f2fc92cecd2925177f0eba93f787d2a041768
PK <33479099+saoter@users.noreply.github.com>
2025-03-25 05:53:46 +0100

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,12 +1,13 @@
 import schedule
 import time
+import subprocess
 
-def task():
-    print("Running scheduled task...")
-    # Your task implementation
+def run_prediction():
+    subprocess.run(["python", "/app/scripts/prediction-api-script.py"])
 
-# Schedule the task to run every day at a specific time
-schedule.every().day.at("07:30").do(task)
+# Schedule the task to run every day at specific times
+schedule.every().day.at("07:30").do(run_prediction)
+schedule.every().day.at("18:20").do(run_prediction)
 
 while True:
     schedule.run_pending()
